6a872421dee7f0c05a31ba3a8a0d7354a5f07c41
F4ckToReal <shirok_dima@mail.ru>
2021-09-20 16:39:29 +0300

    Исправил метод save()

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -6,51 +6,62 @@ import java.util.Arrays;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    Resume[] storage = new Resume[10];
 
     void clear() {
         Arrays.fill(storage,null);
     }
 
     void save(Resume r) {
-            for (int i = 0; i < storage.length; i++) {
-                if (storage[i] == null) {
-                    storage[i] = r;
-                    break;
+        try {
+                for (int i = 0; i < storage.length; i++) {
+                    if (storage[i] == null) {
+                        storage[i] = r;
+                        break;
+                    }
+                    if ((storage[i].toString().equals(r.toString()))) {
+                        System.out.println("РЕЗЮМЕ С ТАКИМ ИМЕНЕМ УЖЕ ЕСТЬ В БАЗЕ");
+                        break;
+                    }
                 }
-            }
+        } catch (NullPointerException ex){
+            ex.printStackTrace();
+        }
     }
 
     Resume get(String uuid) {
         Resume resum = new Resume();
         try {
             for (Resume resume : storage) {
+                if (resume.toString() == null)
+                    break;
                 if (resume.toString().equals(uuid)) {
                     resum = resume;
                 }
             }
         } catch (NullPointerException e) {
+            e.printStackTrace();
         }
         return resum;
     }
 
     void delete(String uuid) {
         int size = size();
-        try {
-            for (int i = 0; i < size; i++) {
+        try  {
+            for (int i = 0; i < storage.length; i++) {
                 if (storage[i].toString().equals(uuid)) {
                     storage[i] = null;
-                    for (int k = i; k < size - 1; k++) {
-                        if (storage[k + 1] != null) {
-                            storage[i] = storage[k + 1];
-                            storage[k + 1] = null;
-                            break;
+                        for (int k = i; k < storage.length - 1; k++) {
+                            if (storage[k + 1] != null) {
+                                storage[i] = storage[k + 1];
+                                storage[k + 1] = null;
+                                break;
+                            }
                         }
                     }
                 }
-            }
-        }
-        catch (NullPointerException ex){
+        } catch (NullPointerException ex) {
+            ex.printStackTrace();
         }
     }
 
