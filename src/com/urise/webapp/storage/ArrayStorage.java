79303d3471a3612c9cf067d73e3302f622386dc8
F4ckToReal <shirok_dima@mail.ru>
2021-10-10 22:07:32 +0300

    разбил по папкам, добавил проверку на переполнение в метод save()

--- src/com/urise/webapp/storage/ArrayStorage.java
+++ src/com/urise/webapp/storage/ArrayStorage.java
@@ -1,37 +1,50 @@
+package com.urise.webapp.storage;
 
-import java.beans.beancontext.BeanContextServiceRevokedEvent;
+import com.urise.webapp.model.Resume;
+
 import java.util.Arrays;
 
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
-    int size = 0;
+    private Resume[] storage = new Resume[10000];
+    private int size = 0;
 
-    void clear() {
+    public void clear() {
         Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 
-    void save(Resume r) {
+    public void save(Resume resum) {
         boolean resumeInBase = false;
-        if (r.toString() != null) {
+        if (resum.toString() != null) {
             for (int i = 0; i < size; i++) {
-                if (storage[i].toString().equals(r.toString())) {
+                if (storage[i].toString().equals(resum.toString())) {
                     System.out.println("Резюме с таким UUID присутствует в базе");
                     resumeInBase = true;
                 }
             }
-            if (!resumeInBase) {
-                storage[size] = r;
+            if ((!resumeInBase) & (size < storage.length)) {
+                storage[size] = resum;
                 size++;
-            }
-        } else System.out.println("Введите uuid!");
+            } else System.out.println("База резюме заполнена");
+        } else System.out.println("Ошибка, вы не ввели uuid");
     }
 
 
-    Resume get(String uuid) {
+    public void update(Resume resume) {
+        if (size > 0) {
+            for (int i = 0; i < size; i++) {
+                if (storage[i].equals(resume)) {
+                    storage[i] = resume;
+                }
+            }
+        } else System.out.println("база пуста");
+    }
+
+
+    public Resume get(String uuid) {
         for (int i = 0; i < size; i++) {
             if (storage[i].toString().equals(uuid)) {
                 return storage[i];
@@ -41,7 +54,7 @@ public class ArrayStorage {
     }
 
 
-    void delete(String uuid) {
+    public void delete(String uuid) {
         int i;
         for (i = 0; i < size; i++) {
             if (storage[i].toString().equals(uuid)) {
@@ -58,13 +71,13 @@ public class ArrayStorage {
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
+    public Resume[] getAll() {
         Resume[] allResume = new Resume[size];
         if (size >= 0) System.arraycopy(storage, 0, allResume, 0, size);
         return allResume;
     }
 
-    int size() {
+    public int size() {
         return size;
     }
 }
