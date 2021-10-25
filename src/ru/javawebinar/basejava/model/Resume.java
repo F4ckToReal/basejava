3ee73933c7441cfd6e140fb5646d04d63f36e7cf
F4ckToReal <shirok_dima@mail.ru>
2021-10-25 10:01:06 +0300

    добавлен пакет exception с 3 классами. Переделаны выбросы исключений в Abstract ArrayStorage

--- src/ru/javawebinar/basejava/model/Resume.java
+++ src/ru/javawebinar/basejava/model/Resume.java
@@ -1,4 +1,5 @@
 package ru.javawebinar.basejava.model;
+import java.util.UUID;
 
 /**
  * Initial resume class
@@ -6,14 +7,18 @@ package ru.javawebinar.basejava.model;
 public class Resume implements Comparable<Resume> {
 
     // Unique identifier
-    private String uuid;
+    private final String uuid;
+
+    public Resume() {
+        this(UUID.randomUUID().toString());
+    }
+
+    public Resume(String uuid) {
+        this.uuid = uuid;
+    }
 
     public String getUuid() {
         return uuid;
-    }
-
-    public void setUuid(String uuid) {
-        this.uuid = uuid;
     }
 
     @Override
