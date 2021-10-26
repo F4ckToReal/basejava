d8d08b0c92c97d1ea3c50cf08ad202bc4c4760a3
F4ckToReal <shirok_dima@mail.ru>
2021-10-27 01:02:33 +0300

    добавлен класс MainReflection

---
+++ src/ru/javawebinar/basejava/MainReflection.java
@@ -0,0 +1,21 @@
+package ru.javawebinar.basejava;
+
+import ru.javawebinar.basejava.model.Resume;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
+public class MainReflection {
+    public static void main(String[] args) throws IllegalAccessException, InvocationTargetException {
+        Resume r = new Resume();
+        Field field = r.getClass().getDeclaredFields()[0];
+        System.out.println(field.getName());
+        field.setAccessible(true);
+        System.out.println(field.get(r));
+        field.set(r, "Yo!");
+        System.out.println(r);
+        Method [] methods = r.getClass().getDeclaredMethods();
+        System.out.println(methods[1].invoke(r));
+    }
+}
