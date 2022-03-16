ce1b296b39aa8f6d4b5272578dfa606bb0249254
F4ckToReal <shirok_dima@mail.ru>
2022-03-16 09:30:17 +0300

    урок 12. TestStreamHomework_12

---
+++ src/ru/javawebinar/basejava/util/TestStreamHomework_12.java
@@ -0,0 +1,36 @@
+package ru.javawebinar.basejava.util;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class TestStreamHomework_12 {
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        System.out.println("Enter array length: ");
+        String lengthMas = reader.readLine();
+        int size = Integer.parseInt(lengthMas); // Читаем с клавиатуры размер массива и записываем в size
+         int [] array = new int[size]; // Создаём массив int размером в size
+        System.out.println("Insert array elements:");
+        /*Пройдёмся по всему массиву, заполняя его*/
+        for (int i = 0; i < size; i++) {
+            array[i] = Integer.parseInt(reader.readLine()); // Заполняем массив элементами, введёнными с клавиатуры
+        }
+        System.out.print("Inserted array elements:");
+        for (int i = 0; i < size; i++) {
+            System.out.print(array[i]); // Выводим на экран, полученный массив
+        }
+        System.out.println();
+        System.out.println(minValue(array));
+    }
+    private static int minValue(int[] values) {
+        return Arrays.stream(values).distinct()
+                .reduce(0, (a,b) -> 10 * a + b);
+    }
+
+}
+
+
