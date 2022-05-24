0cb8aa209f738feceb7a0af587e85d4261f9ed27
F4ckToReal <shirok_dima@mail.ru>
2022-05-25 02:54:30 +0300

    урок 13.3  SqlStorage описал методы , добавил SqlStorageTest

--- src/ru/javawebinar/basejava/storage/SqlStorage.java
+++ src/ru/javawebinar/basejava/storage/SqlStorage.java
@@ -7,6 +7,7 @@ import ru.javawebinar.basejava.sql.ConnectionFactory;
 
 import java.io.IOException;
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class SqlStorage implements Storage {
@@ -35,6 +36,18 @@ public class SqlStorage implements Storage {
 
     @Override
     public void update(Resume r) {
+        try (Connection conn = connectionFactory.getConnection();
+             PreparedStatement pr = conn.prepareStatement("UPDATE resume SET uuid = ?, full_name = ?")) {
+            ResultSet rs = pr.executeQuery();
+            if (!rs.next()) {
+                throw new NotExistStorageException(r.getUuid());
+            }
+            pr.setString(1, r.getUuid());
+            pr.setString(2, r.getFullName());
+            pr.execute();
+        } catch (SQLException e) {
+            throw new StorageException(e);
+        }
 
     }
 
@@ -68,16 +81,42 @@ public class SqlStorage implements Storage {
 
     @Override
     public void delete(String uuid) {
+        try (Connection conn = connectionFactory.getConnection();
+             PreparedStatement pr = conn.prepareStatement("DROP TABLE resume")) {
+            pr.execute();
+        } catch (SQLException e) {
+            throw new StorageException(e);
+        }
 
     }
 
     @Override
     public List<Resume> getAllSorted() throws IOException {
-        return null;
+        try (Connection conn = connectionFactory.getConnection();
+             PreparedStatement pr = conn.prepareStatement("SELECT * FROM resume ORDER BY uuid")) {
+            ResultSet rs = pr.executeQuery();
+            List<Resume> list = new ArrayList<>();
+            if (rs.next()) {
+                Resume r = new Resume(rs.getString("uuid"), rs.getString("full_name"));
+                list.add(r);
+            } else throw new NotExistStorageException(rs.getString("uuid"));
+            return list;
+        } catch (SQLException e) {
+            throw new StorageException(e);
+        }
     }
 
     @Override
     public int size() {
-        return 0;
+        try (Connection conn = connectionFactory.getConnection();
+             PreparedStatement pr = conn.prepareStatement("SELECT count() FROM resume")) {
+            ResultSet rs = pr.executeQuery();
+            if (!rs.next()) {
+                throw new NotExistStorageException(rs.getString("uuid"));
+            }
+            return pr.getUpdateCount();
+        } catch (SQLException e) {
+            throw new StorageException(e);
+        }
     }
 }
